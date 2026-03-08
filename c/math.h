f88d1d33e32fde9cd17807ae833a7a9b8b6426c9
Ahsan Haris <ahsanharis2003@gmail.com>
2026-03-09 01:06:29 +0500

    Update math.h
    added definitions for my_pow_int and my_sqrt

--- c/math.h
+++ c/math.h
@@ -5,4 +5,6 @@ float my_log(float x);
 float my_sin(float x);
 float my_cos(float x);
 float my_tanh(float x);
+float my_pow_int(float x, int n);
 float my_pow(float x, float y);
+float my_sqrt(float x);
