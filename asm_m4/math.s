f7646b0c8eab94a2c0cb8802566448dcf3ff84a7
Ahmed Abdul Rahim <ahmed267aar@gmail.com>
2026-05-26 23:30:03 +0500

    Refactor numerator calculation in math.s

--- asm_m4/math.s
+++ asm_m4/math.s
@@ -389,11 +389,11 @@ v_my_tanh:
     vfmul.vv v16, v8, v8
 
     # Numerator: x * (1.0 + 0.10001*x^2)
-    li t1, 0x3DCCCCD0             # 0.10001
+    li      t1, 0x3DCCCCD0             # 0.10001
     fmv.w.x ft0, t1
-    vfmv.v.f v24, ft0
-    vfmacc.vv v24, v16, v24            # v24 = 0.10001 * x^2
-    li t1, 0x3F800000             # 1.0
+    vfmul.vf v24, v16, ft0             # v24 = 0.10001 * x^2 
+    
+    li      t1, 0x3F800000             # 1.0
     fmv.w.x ft1, t1
     vfadd.vf v24, v24, ft1             # v24 = 1.0 + 0.10001*x^2
     vfmul.vv v24, v8, v24              # Numerator complete
